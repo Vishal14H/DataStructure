ddf2b92ecf5b4bb8a7f51293e99192404f401075
Vishal singh <44376633+Vishal14H@users.noreply.github.com>
2021-07-20 23:20:16 +0530

    Updated Commit

--- src/CircularLinkedList.java
+++ src/CircularLinkedList.java
@@ -13,8 +13,65 @@ public class CircularLinkedList
 
         public Node(int data)
         {
-            data=this.data;
+            this.data=data;
         }
+    }
+    public CircularLinkedList()
+    {
+        this.last=null;
+        this.length=0;
+    }
+
+    public void display()
+    {
+        if(last==null)
+        {
+            return;
+        }
+        Node first =last.next;
+        while(first != last)
+        {
+            System.out.print(first.data+" ");
+            first=first.next;
+        }
+        System.out.print(first.data);
+    }
+
+    public void createcircularLinkedList()
+    {
+        Node first = new Node(1);
+        Node second = new Node(23);
+        Node third = new Node(22);
+        Node fourth = new Node(69);
+
+        first.next=second;
+        second.next=third;
+        third.next=fourth;
+        fourth.next=first;
+
+        last=fourth;
+    }
+
+    public void insetFirst(int data)
+    {
+        Node temp = new Node(data);
+        if(last == null)
+        {
+            last=temp;
+        }
+        else {
+            temp.next = last.next;
+        }
+            last.next=temp;
+            length++;
+        }
+
+
+    public static void main(String[] args) {
+        CircularLinkedList cll = new CircularLinkedList();
+        cll.createcircularLinkedList();
+        cll.insetFirst(9);
+        cll.display();
     }
 
 
