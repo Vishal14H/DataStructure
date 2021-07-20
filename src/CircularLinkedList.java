d0b415d050d4b8fe9205ee810284b4732882db9e
Vishal singh <44376633+Vishal14H@users.noreply.github.com>
2021-07-20 22:20:22 +0530

    Updated Commit

---
+++ src/CircularLinkedList.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class CircularLinkedList
+{
+    private Node last;
+    private int length;
+
+    private class Node
+    {
+        private Node next;
+        private int data;
+
+
+        public Node(int data)
+        {
+            data=this.data;
+        }
+    }
+
+
+}
